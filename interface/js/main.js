010575d1cb6355cdd974996e7545817a5f93165d
SergioGMA <sergiosrs1000@gmail.com>
2022-11-16 19:39:33 -0300

    Mudança no front-end

--- interface/js/main.js
+++ interface/js/main.js
@@ -1,1 +1,10 @@
-console.log("Teste")
+function fonte(e){
+	let elemento = $(".acessibilidade");
+	let fonte = elemento.css('font-size');
+
+	if (e == 'a') {
+		elemento.css("fontSize", parseInt(fonte) + 1);
+	} else if('d'){
+		elemento.css("fontSize", parseInt(fonte) - 1);
+	}
+}
